b1a37138423077a666d870ea8f2866f625ffedd1
Caio Augusto <caioaugusto4520@gmail.com>
2023-03-01 23:14:03 -0300

    Adding messages sender and receiver

---
+++ src/main/java/com/example/demo/Sender.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Sender {
+
+    private static String NAME_QUEUE = "HELLO";
+
+    public static void main(String[] args) throws Exception {
+        // creating the connection
+        // setting creation information
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("admin");
+        factory.setPassword("pass123");
+        factory.setPort(5672);
+
+        Connection connection = factory.newConnection();
+        //System.out.println(connection.hashCode());
+
+        //creating a new channel
+        Channel channel1 = connection.createChannel();
+        System.out.println(channel1);
+
+        //declaring the queue that will be used
+        channel1.queueDeclare(NAME_QUEUE, false, false, false, null);
+
+        //sending the message
+        String message = "Hello World";
+        channel1.basicPublish("", NAME_QUEUE, null, message.getBytes());
+
+        System.out.print("[x] sent  '" + message + "'");
+    }
+}
